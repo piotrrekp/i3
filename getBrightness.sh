3600c34d42031356c5fc604b2c3bdcebc25a2d33
piotrrekp <piotrrek1988@gmail.com>
2019-03-01 20:31:56 +0100

    WIP:walka z xbrigthess

--- getBrightness.sh
+++ getBrightness.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-a=xbacklight -get | awk '{printf "%.0f",$1}'
-notify-send "Brightness: "+$a
+sleep 0.2
+# Get the brightness percentage:
+MAX_BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/max_brightness);
+BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/actual_brightness);
+PCT=$(echo $BRIGHTNESS $MAX_BRIGHTNESS  | awk '{printf "%4.0f\n",($1/$2)*100}')
+notify-send `echo BRIGHTNESS: $PCT `
